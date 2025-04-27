52467082b9ab423a4e5d4f3e4911ac2ebecd2096
Mohamed Nidal <touhami9@gmail.com>
2025-04-27 18:23:32 +0200

    add production environment check for server port configuration

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  if (process.env.NODE_ENV === 'production') {
+    return await app.listen('8080');
+  }
+
   await app.listen(process.env.PORT ?? 5500);
 }
 bootstrap();
